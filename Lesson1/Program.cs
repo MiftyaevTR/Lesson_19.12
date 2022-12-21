abc9402570a243613a528686154560e51680cf54
Timur <t926661227@yandex.ru>
2022-12-21 11:11:50 +0300

    Second exercise

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
+/*
 int Secondnumber(int number)
 {
     int el = number/10;
@@ -13,9 +13,19 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine(Secondnumber(number));
 
-
-
+*/
 //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+Console.Write("Просьба введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string str = num.ToString();
+int strLen = str.Length;
+
+if (strLen<3) Console.WriteLine("Вы ввели слишком маленькое число. Просьба попробуйте снова");
+else Console.WriteLine(str[strLen-3]);
+ 
 
 //Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
