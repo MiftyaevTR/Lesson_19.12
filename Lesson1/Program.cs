c6dcde1998a1c2523b30e8221a563121667f84c1
Timur <t926661227@yandex.ru>
2022-12-22 16:07:14 +0300

    Third exercise

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -15,8 +15,7 @@ Console.WriteLine(Secondnumber(number));
 
 */
 //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-
-
+/*
 Console.Write("Просьба введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
@@ -25,10 +24,24 @@ int strLen = str.Length;
 
 if (strLen<3) Console.WriteLine("Вы ввели слишком маленькое число. Просьба попробуйте снова");
 else Console.WriteLine(str[strLen-3]);
- 
+ */
 
 //Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+/*
+bool Weekday(int num)
+{
+    if (num==6 || num ==7)
+    {
+        return true;
+    } 
+    else 
+    {
+        return false;
+    }
+}
 
+Console.WriteLine ("Просьба введите число от 1 до 7!");
+int num = Convert.ToInt32(Console.ReadLine());
 
-
-
+Console.WriteLine(Weekday(num));
+*/
