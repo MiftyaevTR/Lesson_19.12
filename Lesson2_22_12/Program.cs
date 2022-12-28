f099ed0a7502796d20e7e5a6c83362365edb90be
Timur <t926661227@yandex.ru>
2022-12-28 20:58:31 +0300

    Вторая задача

--- Lesson2_22_12/Program.cs
+++ Lesson2_22_12/Program.cs
@@ -10,5 +10,21 @@ if (str[0]==str[4] && str[1]==str[3]) Console.WriteLine("Данное число
 else Console.WriteLine("Данное число не является полиндромом");
 */
 
+// Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
+void Cub (int num)
+{
+    int count = 1;
+    while (count<num + 1)
+    {
+        Console.Write(count*count*count + " ");
+        count=count+1;
+    }
+}
+
+Console.Write("Просьба введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Cub(num);
