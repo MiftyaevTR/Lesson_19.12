febda82933c40d7114b508e0b72692c03b81adaa
Timur <t926661227@yandex.ru>
2022-12-28 21:28:15 +0300

    Третья задача

--- Lesson2_22_12/Program.cs
+++ Lesson2_22_12/Program.cs
@@ -11,9 +11,31 @@ else Console.WriteLine("Данное число не является полин
 */
 
 // Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+/*
+Console.Clear();
+double Distance(double x1, double y1, double z1,
+                double x2, double y2, double z2)
+{
+    double dis = Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2)+Math.Pow(z1-z2,2));
+    return dis;
+}
 
+Console.Write("Введите координаты первой точки: ");
+double x1 =Convert.ToDouble(Console.ReadLine());
+double y1 =Convert.ToDouble(Console.ReadLine());
+double z1 =Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координаты второй точки: ");
+double x2 =Convert.ToDouble(Console.ReadLine());
+double y2 =Convert.ToDouble(Console.ReadLine());
+double z2 =Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Расстояние между точками " + Distance(x1, y1, z1, x2, y2, z2 ));
+ 
+
+*/
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-
+/*
 void Cub (int num)
 {
     int count = 1;
@@ -28,3 +50,5 @@ Console.Write("Просьба введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 Cub(num);
+*/
+
